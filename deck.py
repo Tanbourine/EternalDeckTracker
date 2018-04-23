a0f3bfc6defe04f81eda42c6fc7e4d64a027bfc9
Darren Tan <dyztan@gmail.com>
2018-04-22 21:19:44 -0700

    4 basic sorts

--- deck.py
+++ deck.py
@@ -1,4 +1,4 @@
-""" deck object to self sort ALPHA, TYPE_cost_alpha, COST_alpha
+""" deck object to self sort ALPHA, TYPE_alpha_cost, TYPE_cost_alpha, COST_alpha
     caps is major category, lower is tiebreakers"""
 
 import card as cd
@@ -22,6 +22,39 @@ class Deck():
         alpha_arr.sort()
         return alpha_arr
 
+    def type_alpha_cost(self):
+        """ sorts by type then alpha then cost """
+        holding_arr = []
+        spells_arr = []
+        power_arr = []
+        units_arr = []
+
+        for card in self.decklist:
+            holding_arr.append([card.name(), card.type(), card.cost()])
+
+        # organize by type
+        for card in holding_arr:
+            if card[1].lower() in ["spell", "fast spell", "curse", "cursed relic",
+                                   "relic"]:
+                spells_arr.append(card)
+
+            elif card[1].lower() in ["power"]:
+                power_arr.append(card)
+
+            else:
+                units_arr.append(card)
+
+        spells_arr.sort()
+        power_arr.sort()
+        units_arr.sort()
+
+        print(spells_arr)
+        print('------------')
+        print(power_arr)
+        print('------------')
+        print(units_arr)
+        print('------------')
+
     def type_cost_alpha(self):
         """ sorts by type then color then alpha """
         holding_arr = []
@@ -32,18 +65,17 @@ class Deck():
         for card in self.decklist:
             holding_arr.append([card.name(), card.type(), card.cost()])
 
-        # organize into spells
+        # organize by type
         for card in holding_arr:
-            card[1] = card[1].lower()
-            if card[1] in ["spell", "fast spell", "curse", "cursed relic",
-                           "relic"]:
-                spells_arr.append(card[0])
+            if card[1].lower() in ["spell", "fast spell", "curse", "cursed relic",
+                                   "relic"]:
+                spells_arr.append(card)
 
-            elif card[1] in ["power"]:
-                power_arr.append(card[0])
+            elif card[1].lower() in ["power"]:
+                power_arr.append(card)
 
             else:
-                units_arr.append(card[0])
+                units_arr.append(card)
 
         # print(spells_arr)
         # print('------------')
@@ -61,7 +93,8 @@ def main():
 
     deck = Deck(keyed_decklist, card_db)
     # alpha_deck = deck.alpha_sort()
-    type_deck = deck.type_cost_alpha()
+    deck.type_cost_alpha()
+    deck.type_alpha_cost()
 
 
 if __name__ == "__main__":
