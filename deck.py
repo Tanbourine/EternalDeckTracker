2e716847840045ddea1f2b93b93382e201f2efcc
Darren Tan <dyztan@gmail.com>
2018-04-22 20:59:44 -0700

    type_cost_alpha initial

--- deck.py
+++ deck.py
@@ -1,5 +1,5 @@
-""" deck object to self sort ALPHA, TYPE_cost_alpha, COST_alpha 
-caps is major category, lower is tiebreakers"""
+""" deck object to self sort ALPHA, TYPE_cost_alpha, COST_alpha
+    caps is major category, lower is tiebreakers"""
 
 import card as cd
 
@@ -25,11 +25,32 @@ class Deck():
     def type_cost_alpha(self):
         """ sorts by type then color then alpha """
         holding_arr = []
+        spells_arr = []
+        power_arr = []
+        units_arr = []
 
         for card in self.decklist:
             holding_arr.append([card.name(), card.type(), card.cost()])
 
-        print(sorted_arr)
+        # organize into spells
+        for card in holding_arr:
+            card[1] = card[1].lower()
+            if card[1] in ["spell", "fast spell", "curse", "cursed relic",
+                           "relic"]:
+                spells_arr.append(card[0])
+
+            elif card[1] in ["power"]:
+                power_arr.append(card[0])
+
+            else:
+                units_arr.append(card[0])
+
+        # print(spells_arr)
+        # print('------------')
+        # print(power_arr)
+        # print('------------')
+        # print(units_arr)
+        # print('------------')
 
 
 def main():
