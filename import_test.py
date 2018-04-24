f8b27bd0c7667aead2d331a5fe345e7e1a749c3f
Darren Tan <darren.tan@spacex.com>
2018-04-24 10:34:04 -0700

    simplified import_test

--- import_test.py
+++ import_test.py
@@ -9,12 +9,9 @@ CARD_DB = 'eternal-cards-1.31.json'
 def main():
     """ main """
 
-    deck= cd.import_deck(DECKLIST)
-    card_db = cd.import_json(CARD_DB)
-    keyed_decklist = cd.create_keyed_decklist(deck, card_db)
 
-    deck = dk.Deck(keyed_decklist, card_db)
-    print(deck.deck)
+    deck = dk.Deck(DECKLIST, CARD_DB)
+    print(deck.card_names())
 
 
 
