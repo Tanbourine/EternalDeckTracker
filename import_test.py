8c2ce86cd2fca7d20739fedbe711a41438cf7d07
Darren Tan <darren.tan@spacex.com>
2018-04-24 17:17:26 -0700

    pretty printing

--- import_test.py
+++ import_test.py
@@ -1,6 +1,7 @@
 """ testing importing """
 import deck as dk
 import card as cd
+import pprint
 
 DECKLIST = 'deck.csv'
 CARD_DB = 'eternal-cards-1.31.json'
@@ -11,15 +12,32 @@ def main():
 
 
     deck = dk.Deck(DECKLIST, CARD_DB)
-    print(deck.card_names())
-    print('First card of deck is:', deck.deck_obj[0].name())
-    print('Its cost is:',deck.deck_obj[0].cost())
-    print('Its probability to draw in this deck is:', '{0: .2f}'.format(deck.probability()[0]))
 
-    deck.add_card(0,0)
-    print('Its probability to draw in this deck is:', '{0: .2f}'.format(deck.probability()[0]))
+    card_type = 0
+    card_index = 3
 
-    # ISSUE WITH DECK_OBJ NOT BEING SORTED. Might reconsider restructuring to perform all objects on the object
+    print('Selected card is:', deck.deck[card_type][card_index].name)
+    print('Its cost is:',deck.deck[card_type][card_index].cost)
+    print('Its rarity is:',deck.deck[card_type][card_index].rarity)
+    print('Its influence requirement is:',deck.deck[card_type][card_index].influence)
+    print('Its probability to draw in this deck is:', '{0: .2f}'.format(deck.deck[card_type][card_index].probability))
+
+    print('')
+
+    deck.add_card(card_type, card_index)
+    print('Its probability to draw in this deck is:', '{0: .2f}'.format(deck.deck[card_type][card_index].probability))
+
+    print('')
+    print('')
+    print('Printing list of all selected parameters')
+    deck.deck_sort('type_alpha')
+    print('List is sorted by:', deck.sort_method)
+    print('')
+    print('')
+
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(deck.show_property('Name', 'Cost', 'Rarity'))
+
 
 
 
