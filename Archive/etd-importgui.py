c7f7459b68cb78ed8dc99ffcd7cf8887236aa9e9
Amanda Tan <tan.amanda.j@gmail.com>
2018-04-30 10:01:02 -0700

    Created etd-imiportgui.py

---
+++ Archive/etd-importgui.py
@@ -0,0 +1,12 @@
+#allows user to copy/paste exported deck from eternal into entry box to create a deck
+
+class page2(tk.Frame):
+    """page 2 window/application frame. Entry box, button, and label within"""
+
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+
+        self.master = master
+        self.create_widget()
+        self.poop = 0
+        
